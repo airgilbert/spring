1bf4c19ad015e73910375fe51ae03d214d4178c4
airgilbert <airgilbert@example.com>
2018-10-25 15:48:01 +0800

    提交信息

---
+++ src/main/java/com/air/spring/SpringBootDemo1Application.java
@@ -0,0 +1,14 @@
+package com.air.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo1Application {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemo1Application.class, args);
+    }
+}
